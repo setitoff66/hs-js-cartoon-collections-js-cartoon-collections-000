efac313fb81f9a6d788a397613cdc5c5560ad0d1
Sean Hendry <sean.c.hendry@gmail.com>
2018-12-13 22:52:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 function dwarfRollCall(dwarves) {
- }
+   var array = []
+   for (var i=0;i<dwarves.length;i++){
+   	array.push(` ${i+1}. $(dwarves[i]}`)
+   } 
+  return array.join("")
 
+}
 
 function summonCaptainPlanet(planeteerCalls){
+ return planeteerCalls.map(a=>a.toUpperCase()+"!")
 }
-
-function longPlaneteerCalls(words) {
+ 
+function longPlaneteerCalls(words){
+  for(var i=0;i<words.length;i++){
+    if (words[i].length>4){
+      return true
+    }
+  } 
+  
+  return false
 }
+ 
 
 function findTheCheese (foods) {
+  for(var i=0;i<foods.length;i++){
+    if (foods[i]=="cheddar"
+    ||foods[i]=="gouda"
+    ||foods[i]=="camembert"
+    ){
+    return foods[i]
+  }
 }
+return "no cheese!"
+}
+
