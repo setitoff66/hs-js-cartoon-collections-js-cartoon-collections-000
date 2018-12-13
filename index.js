27991517573126d060c7621250819a603609235f
Sean Hendry <sean.c.hendry@gmail.com>
2018-12-13 20:26:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function dwarfRollCall(dwarves) {
+  var array = [];
+  for (var i=0;  dwarves.length; i++){
+   array.push. dwarves[i] }
+  return array.join()
 }
+  }
+}
+
 
 function summonCaptainPlanet(planeteerCalls){
 }
