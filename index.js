1af7e872e3886f56ab8d04a1688e968b3dcf1d6d
Sean <37030007+setitoff66@users.noreply.github.com>
2018-12-13 15:55:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 function dwarfRollCall(dwarves) {
-  var array = [];
-  for (var i=0;  dwarves.length; i++){
-   array.push. dwarves[i] }
-  return array.join()
-}
-  }
-}
+ }
 
 
 function summonCaptainPlanet(planeteerCalls){
